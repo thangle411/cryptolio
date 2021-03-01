b70a47332982a5352b0d65d71111909924e56d6a
Thang-Le-T <77654827+Thang-Le-T@users.noreply.github.com>
2021-02-28 19:11:59 -0800

    Update coins.ts

--- src/assets/coins/coins.ts
+++ src/assets/coins/coins.ts
@@ -1,9 +1,4 @@
 export const coins = [
-    {
-        "name": "MRPH",
-        "pairAddress": "0xb89ed43f45d1f870be815c7b53368a1cc5184474",
-        "coinAddress": "0x7b0c06043468469967dba22d1af33d77d44056c8"
-    },
     {   
         "name": "MITX",
         "pairAddress": "0x51fe8e8fa3f9f10288fa8c2aff1400f887d21c42",
@@ -23,11 +18,6 @@ export const coins = [
         "name":"GTH",
         "pairAddress": "0xb38be7fd90669abcdfb314dbddf6143aa88d3110",
         "coinAddress": "0xc3771d47e2ab5a519e2917e61e23078d0c05ed7f"
-    },
-    {   
-        "name": "THIRM",
-        "pairAddress": "0xd912aa12e3cf3458d478da3c629096f4e8bd5f72",
-        "coinAddress": "0xb526fd41360c98929006f3bdcbd16d55de4b0069"
     },
     {
         "name":"QNT",
@@ -50,11 +40,6 @@ export const coins = [
         "coinAddress": "0x24e3794605c84e580eea4972738d633e8a7127c8"
     },
     {
-        "name":"XFUND",
-        "pairAddress": "0xab2d2f5bc36620a57ec4bb60d6a7df2a847deab5",
-        "coinAddress": "0x892a6f9df0147e5f079b0993f486f9aca3c87881"
-    },
-    {
         "name":"BUND",
         "pairAddress": "0xed86244cd91f4072c7c5b7f8ec3a2e97ea31b693",
         "coinAddress": "0x8d3e855f3f55109d473735ab76f753218400fe96"
@@ -64,9 +49,4 @@ export const coins = [
         "pairAddress": "0xc0bf97bffa94a50502265c579a3b7086d081664b",
         "coinAddress": "0x990f341946a3fdb507ae7e52d17851b87168017c"
     },
-    {
-        "name":"ORAI",
-        "pairAddress": "0x9081b50bad8beefac48cc616694c26b027c559bb",
-        "coinAddress": "0x4c11249814f11b9346808179cf06e71ac328c1b5"
-    }
 ]
