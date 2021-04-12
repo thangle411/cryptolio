a25567f486198d11719eb33e66f3b0bf32d8b9d7
Thang-Le-T <77654827+Thang-Le-T@users.noreply.github.com>
2021-04-12 15:46:00 -0700

    Update coins.ts

--- src/assets/coins/coins.ts
+++ src/assets/coins/coins.ts
@@ -65,11 +65,6 @@ export const coins = [
         "coinAddress": "0x07bac35846e5ed502aa91adf6a9e7aa210f2dcbe"
     },
     {
-        "name":"BOSON",
-        "pairAddress": "0xe8edd9968245873176e30548147efdd13e0b22f4",
-        "coinAddress": "0xc477d038d5420c6a9e0b031712f61c5120090de9"
-    },
-    {
         "name":"VSP",
         "pairAddress": "0x6d7b6dad6abed1dfa5eba37a6667ba9dcfd49077",
         "coinAddress": "0x1b40183efb4dd766f11bda7a7c3ad8982e998421"
